f9d7e6dd5b03578cbbb69530acca9335fbf68b80
Quynh Trang <trang98vp@gmail.com>
2019-06-01 20:31:45 +0700

    Program.cs

--- Face_recognition/Face_recognition/Program.cs
+++ Face_recognition/Face_recognition/Program.cs
@@ -1,91 +1,106 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Face_recognition
 {
     class Program
     {
-        static string faceAPIkey = "6b90778a91a34a1cac88c01c912dc579";
-        const string faceEndpoint = "https://southeastasia.api.cognitive.microsoft.com/face/v1.0";
-        const string personGroudId = "face";
-        const string Sinh_vien_img_dir = @"D:\picture\QuynhTrang";
+        static string _faceAPIKey = "19a4f858125e4f46bf02fd8c8146cf57";
+        const string faceEndpoint = "https://eastasia.api.cognitive.microsoft.com";
+        const string personGroupId = "myfriends";
+        const string friendImageDir = @"D:\picture\APIFace\QuynhTrang";
         static void Main(string[] args)
         {
-            var faceClient = new FaceClient(new ApiKeyServiceClientCredentials(faceAPIkey),
-                                              new System.Net.Http.DelegatingHandler[] { });
+            var faceClient = new FaceClient(new ApiKeyServiceClientCredentials(_faceAPIKey),
+                                            new System.Net.Http.DelegatingHandler[] { });
             faceClient.Endpoint = faceEndpoint;
 
-            try
+            //Create a new PersonGroup
+
+            #region tạo group mới, nếu không tạo thì dùng groupid đã có
+            var faceId = faceClient.PersonGroup.CreateAsync("myfriends", "My Friends").GetAwaiter(); // không có getResult
+            Console.WriteLine(faceId.IsCompleted);
+            Console.WriteLine("Create successful");
+            #endregion
+
+            #region Tạo một person mới trong group
+            //Add a person to person group 
+            var studentTrang = faceClient.PersonGroupPerson.CreateAsync(personGroupId, "Nguyen Thi Quynh Trang - 20164169").GetAwaiter().GetResult();
+            string trangid = studentTrang.PersonId.ToString();
+            Console.WriteLine("id " + trangid);
+            #endregion
+           
+            #region add thêm ảnh vào Person đã có
+            //// nếu cần phải add thêm ảnh
+            string getTrangid = "ec0aec29-5a6f-465c-b93f-d900820f7a41";
+            Person trang = new Person(new Guid(getTrangid));
+            Console.WriteLine(trang.PersonId);
+            Console.WriteLine("Add successful");
+
+            string[] array = Directory.GetFiles(friendImageDir, "*.jpg");
+            for (int i = 0; i < array.Length; i++)
             {
-                //Create a new PersonGroup
-                var faceId = faceClient.PersonGroup.CreateAsync("sinhVien", "SinhVien");
-                Console.WriteLine("Create personGroup");
-
-
-                //add person to personGroup
-                string personName = "QuynhTrang";
-                var Quynh_Trang = faceClient.PersonGroupPerson.CreateAsync(personGroudId, personName).GetAwaiter().GetResult();
-                Console.WriteLine("Add person to personGroup");
-
-                //Add Face
-                foreach (var img in Directory.GetFiles(Sinh_vien_img_dir, "*.*"))
+                string image = array[i];
+                using (Stream imageStream = File.OpenRead(image))
                 {
-                    using (Stream imageStream = File.OpenRead(img))
-                    {
-                        faceClient.PersonGroupPerson.AddFaceFromStreamAsync(personGroudId, Quynh_Trang.PersonId, imageStream).GetAwaiter().GetResult();
-                        Console.WriteLine("Add Person to PersonGroup successful");
-
-                    }
+                    var r = faceClient.PersonGroupPerson.AddFaceFromStreamAsync(personGroupId, studentTrang.PersonId, imageStream);
+                    Console.WriteLine(r.Result);
+                    Console.WriteLine("Add Face Successful");
                 }
             }
-            catch (Exception ex)
+            #endregion
+
+            #region training group
+            faceClient.PersonGroup.TrainAsync(personGroupId).GetAwaiter().GetResult();
+            TrainingStatus training = null;
+            while (true)
             {
-                Console.WriteLine("ERROR");
+                training = faceClient.PersonGroup.GetTrainingStatusAsync(personGroupId).GetAwaiter().GetResult();
+                Console.WriteLine("Status" + training.Status);
+                if (training.Status == TrainingStatusType.Succeeded)
+                {
+                    break;
+                }
             }
+            Console.WriteLine("Training Successful");
+            #endregion
+            
 
-            //trainning
-            faceClient.PersonGroup.TrainAsync(personGroudId);
-            Console.WriteLine("training successfull");
-
-
-            //Identify the face
-            string testImg = @"D:\picture\my.jpg";
-            using (Stream imgStream = File.OpenRead(testImg))
+            #region Identify the face
+            string testImage = @"D:\picture\Trang\01.jpg";
+            using (Stream imageStream = File.OpenRead(testImage))
             {
-                var faces = faceClient.Face.DetectWithStreamAsync(imgStream, true).GetAwaiter().GetResult();
-                var faceids = faces.Select(e => (Guid)e.FaceId).ToArray();
-                var identifyResults = faceClient.Face.IdentifyAsync(faceids, personGroudId).GetAwaiter().GetResult();
+                var faces = faceClient.Face.DetectWithStreamAsync(imageStream, true).GetAwaiter().GetResult();
+                var faceids = faces.Select(e => (Guid)e.FaceId).ToList();
                 try
                 {
-                    
+                    var identifyResults = faceClient.Face.IdentifyAsync(faceids, personGroupId).GetAwaiter().GetResult();
+
                     foreach (var result in identifyResults)
                     {
                         if (result.Candidates.Count == 0)
-                        {
                             Console.WriteLine("No one identified");
-                        }
                         else
                         {
-
                             var candidateId = result.Candidates[0].PersonId;
-                            var person = faceClient.PersonGroupPerson.GetAsync(personGroudId, candidateId).GetAwaiter().GetResult();
+                            var person = faceClient.PersonGroupPerson.GetAsync(personGroupId, candidateId)
+                            .GetAwaiter().GetResult();
                             Console.WriteLine("Identified as {0}", person.Name);
                         }
                     }
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("ERROR");
-
+                    Console.WriteLine(ex);
                 }
-
             }
+            #endregion
+
+            Console.ReadLine();
         }
+
     }
 }
